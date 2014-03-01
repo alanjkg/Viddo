e0482657db6d6ff41aeb0a460602fb3ef5e9dbb1
Kareem <kareem.kwong@gmail.com>
2014-03-01 16:07:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Viddo::Application.config.secret_key_base = 'f82a12da96fb9b96b340d39bef01bd1f62dc4bbfa856d14f34be4ae98287978870d04ae80651770a5efb7d831a46adc954591bfff7f589357c9fb767113b8179'
