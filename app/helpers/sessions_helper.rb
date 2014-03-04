36b3fd6fcb4e06875e27cba8b15cbd1efabcf664
alanjkg <alanjkg@gmail.com>
2014-03-04 18:35:15 -0500

    omit the partial _field and fixed token

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,47 +1,47 @@
 module SessionsHelper
 
 	def sign_in(user)
-		rememeber_token = User.new_remember_token
+		remember_token = User.new_remember_token
 		cookies.permanent[:remember_token] = remember_token
 		user.update_attribute(:remember_token, User.encrypt(remember_token))
 		self.current_user = user
-	end 
+	end
 
 	def signed_in?
 		!current_user.nil?
-	end 
+	end
 
 	def current_user=(user)
 		@current_user = user
-	end 
+	end
 
 	def current_user
 		remember_token = User.encrypt(cookies[:remember_token])
 		@current_user ||= User.find_by(remember_token: remember_token)
-	end 
+	end
 
 	def signed_in_user
 		unless signed_in?
 			store_location
 			redirect_to signin_url, notice: "Please sign in."
-		end 
-	end 
+		end
+	end
 
 	def sign_out
 		current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
 		cookies.delete(:remember_token)
 		self.current_user = nil
-	end 
+	end
 
 	def redirect_back_or(default)
 		redirect_to(session[:return_to] || default)
 		session.delete(:return_to)
-	end 
+	end
 
 	def store_location
 		session[:return_to] = request.url if request.get?
-	end 
-end 
+	end
+end
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,23 +7,24 @@ class User < ActiveRecord::Base
 
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i   #validating the email format with a regular expression
-	validates :email, presence: true, format: { with:VALID_EMAIL_REGEX },
+	validates :email, presence: true,
+					  format: { with: VALID_EMAIL_REGEX },
 					  uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 6 }
 
 	def User.new_remember_token
 		SecureRandom.urlsafe_base64
-	end 
+	end
 
 	def User.encrypt(token)
 		Digest::SHA1.hexdigest(token.to_s)
-	end 
+	end
 
-	private 
+	private
 
 		def create_remember_token
 			self.remember_token = User.encrypt(User.new_remember_token)
-		end 
+		end
 
 end
