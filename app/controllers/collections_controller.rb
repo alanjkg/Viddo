17c77dc668a4cb13da6a8080972593111d1cae6c
Richard <Richard.cw.li7@gmail.com>
2014-03-06 17:29:18 -0500

    worked one collection-views; made collections index and new pages

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,24 +1,27 @@
 class CollectionsController < ApplicationController 
-	before_action :signed_in_user, only: [:create, :destroy]
+	before_action :signed_in_user, only: [:create, :destroy, :new]
 	before_action :correct_user, only: :destroy
 
 	def new
+		@collection = Collection.new
 	end 
 
 	def index
+		@collections = Collection.all 
 	end 
 
 	def create 
 		@collection = current_user.collections.build(collection_params)
 		if @collection.save
 			flash[:success] = "Collection created!"
-			redirect_to root_url
+			redirect_to collections_url
 		else
 			render 'static_pages/home'
 		end 
 	end 
 
 	def show
+		@collection = Collection.find(params[:id])
 	end 
 
 	def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Viddo::Application.routes.draw do
   resources :users 
   resources :sessions, only: [:new, :create, :destroy]
-
+  resources :collections
 
   root 'pages#home'
 
@@ -11,5 +11,7 @@ Viddo::Application.routes.draw do
   match '/about',       to: 'pages#about',          via: 'get'
   match '/contact',     to: 'pages#contact',        via: 'get'
   match '/join',	    to: 'pages#join',			via: 'get'
+  match '/collections', to: 'collections#index',    via: 'get'
+  
 
 end
