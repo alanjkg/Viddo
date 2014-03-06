ad0ec8117be1d3cd9926d446d92fb59d44ff6cf1
alanjkg <alanjkg@gmail.com>
2014-03-06 12:24:33 -0500

    fixed routing issues

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,4 +2,28 @@ class VideosController < ApplicationController
   def index
 
   end
+
+  def create
+  	@video = current_user.collection.videos.build(video_params)
+  	if @video.save
+  		flash[:success] = "Video made!"
+  		redirect_to root_url
+  	else
+  		render 'static_pages/home'
+  	end
+  end
+
+  private
+
+		def video_params
+			params.require(:video).permit(:user_id)
+		end
+
+		def correct_user
+			@video = current_user.collection.videos.find_by(id: perams[:id])
+			redirect_to root_url if @video.nil?
+		end
+
+
 end
+
