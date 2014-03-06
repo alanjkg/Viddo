db9d1a828801ff68dc749c7710552f52b4cd1211
Richard <Richard.cw.li7@gmail.com>
2014-03-06 13:52:37 -0500

    added validations to collection model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,7 @@
 class Collection < ActiveRecord::Base
 	has_and_belongs_to_many :videos
 	belongs_to :user
+
+	validates :title, presence: true, length: { maximum: 70 }
+	validates :email, uniqueness: true 
 end
