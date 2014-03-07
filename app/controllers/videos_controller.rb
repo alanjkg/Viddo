92e9e756d7c61ce5bff5c74549ced992a754b1a2
Kareem <kareem.kwong@gmail.com>
2014-03-07 17:45:48 -0500

    Mina's part

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < ApplicationController
-  before_action :grab_videos
+  before_action :grab_videos, only: :index
   def index   
   end
 
@@ -10,7 +10,7 @@ class VideosController < ApplicationController
   	@video = Video.new
     @video.title = @search_response.snippet.title
     @video.description = @search_response.snippet.description
-    @video.youtube.id = @search_response.id
+    @video.youtube_id = @search_response.id
   	if @video.save
   		flash[:success] = "Video made!"
   		redirect_to root_url
@@ -26,7 +26,7 @@ class VideosController < ApplicationController
 		end
 
 		def correct_user
-			@video = current_user.collection.videos.find_by(id: perams[:id])
+			@video = current_user.collection.videos.find_by(id: params[:id])
 			redirect_to root_url if @video.nil?
 		end
 
@@ -37,5 +37,19 @@ class VideosController < ApplicationController
           :api_method => YOUTUBE.videos.list,
           :parameters => {:part => 'id,snippet', :videoCategoryId => '17', :chart => 'mostPopular'}
         )
+
+      @youtube_video = @search_response.data.items[rand(0..4)]
+
+      @video = Video.find_by_youtube_id(@youtube_video.id)
+
+      unless @video
+        new_video = Video.new
+        new_video.title = @youtube_video.snippet.title
+        new_video.description = @youtube_video.snippet.description
+        new_video.youtube_id = @youtube_video.id
+        new_video.save
+
+        @video = new_video
+      end
     end
 end
